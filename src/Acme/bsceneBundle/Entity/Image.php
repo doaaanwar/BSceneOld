4b5eab3dc2710d4c9c2914402f1feacb9b30c578
Mahmoud Jallala <mjallala@gmail.com>
2015-03-16 17:15:51 -0400

    Image entities

--- src/Acme/bsceneBundle/Entity/Image.php
+++ src/Acme/bsceneBundle/Entity/Image.php
@@ -5,8 +5,37 @@
  * The entity for the Image object
  * Revision History:
  *      16.03.2015: created, Mahmoud Jallala
- * and open the template in the editor.
  */
-
 //src/bsceneBundle/Entity/Image.php
 
+namespace Acme\bsceneBundle\Entity;
+
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="Image")
+ */
+class Image
+{
+     /**
+     * @ORM\Column(type="integer", length=5)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    
+    
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    protected $logoURL;
+    
+    
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    protected $logoName;
+}
