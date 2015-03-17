1fb84911392b4696b6e022b0cc6ffb7c815f37d1
Doaa Anwar <doaa@iMac.local>
2015-03-17 17:14:02 -0400

    test

---
+++ app/cache/dev/twig/57/e7/6cebaaf04a0ddeb3898422dd1cbd197113642c563f1ff909cac574ed6336.php
@@ -0,0 +1,65 @@
+<?php
+
+/* AcmebsceneBundle:Default:index.html.twig */
+class __TwigTemplate_57e76cebaaf04a0ddeb3898422dd1cbd197113642c563f1ff909cac574ed6336 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        // line 1
+        try {
+            $this->parent = $this->env->loadTemplate("base.html.twig");
+        } catch (Twig_Error_Loader $e) {
+            $e->setTemplateFile($this->getTemplateName());
+            $e->setTemplateLine(1);
+
+            throw $e;
+        }
+
+        $this->blocks = array(
+            'body' => array($this, 'block_body'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return "base.html.twig";
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->parent->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 2
+    public function block_body($context, array $blocks = array())
+    {
+        // line 3
+        echo "    ";
+        $this->displayParentBlock("body", $context, $blocks);
+        echo "
+    
+    Hello ";
+        // line 5
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "AcmebsceneBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  45 => 5,  39 => 3,  36 => 2,  11 => 1,);
+    }
+}
