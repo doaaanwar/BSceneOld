021491cd580c62f30eafa7f99faba28fcf7165ac
doaa <doaa@localhost>
2015-03-17 21:40:11 -0400

    starting login page

--- src/Acme/bsceneBundle/Controller/DefaultController.php
+++ src/Acme/bsceneBundle/Controller/DefaultController.php
@@ -3,11 +3,39 @@
 namespace Acme\bsceneBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('AcmebsceneBundle:Default:index.html.twig', array('name' => $name));
+    }
+    
+    
+     public function loginAction(Request $request)
+    {
+        
+        if($request->getMethod()=='POST'){
+            $username = $request->get('username');
+            $password = $request->get('password');
+            $em = $this->getDoctrine()->getEntityManager();
+            $repository = $em->getRepository('\Acme\bsceneBundle\Entity\Account');
+            $user = $repository->findOneBy(array('username'=>$username,'password'=>$password));
+            if($user)
+            {
+                 return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => $user->getUsername()));
+            }
+            else
+            {
+                 return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => 'login failed'));
+            }
+        }
+       
+       
+        else
+        {
+             return $this->render('AcmebsceneBundle:Default:index.html.twig');
+        }
     }
 }
