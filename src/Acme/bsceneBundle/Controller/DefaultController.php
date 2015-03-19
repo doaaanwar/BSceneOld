ce231331bb58eed1ffeba0fafc5bca40cbd64195
doaa <doaa@localhost>
2015-03-19 11:51:20 -0400

    handle session

--- src/Acme/bsceneBundle/Controller/DefaultController.php
+++ src/Acme/bsceneBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Acme\bsceneBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class DefaultController extends Controller
 {
@@ -29,11 +30,24 @@ class DefaultController extends Controller
             $user = $repository->findOneBy(array('username'=>$username,'password'=>$password));
             if($user)
             {
+                $session = new Session();
+                $session->start();
+                $session->invalidate(50);
+                $session->set('member',$user->getUsername());
+                $session->set('memberId',$user->getId());
+                if($user->getIsAdmin() == 1)
+                {
+                    
+                    $session->set('admin','admin');
+                }
+               
+                 //$user->getRoles();
                  return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => $user->getUsername()));
             }
             else
             {
                  return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => 'login failed'));
+             
             }
         }
        
