6382f5e027555a1410ef037254060af72f7da25e
vbetts <toribetts@gmail.com>
2015-03-19 16:25:07 -0400

    added Organization CRUD

--- src/Acme/bsceneBundle/Controller/OrganizationController.php
+++ src/Acme/bsceneBundle/Controller/OrganizationController.php
@@ -1,44 +1,224 @@
 <?php
 
-/**
- * OrganizationController.php
- * controller used to manage operation on the organization object
- * 
- * Revision History:
- *      19.03.2015: created, doaa elfayoumi
- * 
- */
-
 namespace Acme\bsceneBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
+use Acme\bsceneBundle\Entity\Organization;
+use Acme\bsceneBundle\Form\OrganizationType;
 
-
+/**
+ * Organization controller.
+ *
+ */
 class OrganizationController extends Controller
 {
-    /*
-     * function that get the list of organization and go to the organization list page
+
+    /**
+     * Lists all Organization entities.
+     *
      */
     public function indexAction()
     {
-        $em = $this->getDoctrine()->getEntityManager();
-        $repository = $em->getRepository('\Acme\bsceneBundle\Entity\Organization');
+        $em = $this->getDoctrine()->getManager();
 
-        
-        $organizationList = $repository->findAll();
-        if(count($organizationList) > 0)
-        {
-        
-            return $this->render('AcmebsceneBundle:Organization:organizationList.html.twig',array('orgList' => $organizationList));
-        }
-        else 
-        {
-            return $this->render('AcmebsceneBundle:Organization:organizationList.html.twig',array('orgList' => NULL,'errormessage' => "No organization list found"));
+        $entities = $em->getRepository('AcmebsceneBundle:Organization')->findAll();
 
-        }
+        return $this->render('AcmebsceneBundle:Organization:index.html.twig', array(
+            'entities' => $entities,
+        ));
     }
-    
+    /**
+     * Creates a new Organization entity.
+     *
+     */
+    public function createAction(Request $request)
+    {
+        $entity = new Organization();
+        $form = $this->createCreateForm($entity);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('organization_show', array('id' => $entity->getId())));
+        }
+
+        return $this->render('AcmebsceneBundle:Organization:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Creates a form to create a Organization entity.
+     *
+     * @param Organization $entity The entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createCreateForm(Organization $entity)
+    {
+        $form = $this->createForm(new OrganizationType(), $entity, array(
+            'action' => $this->generateUrl('organization_create'),
+            'method' => 'POST',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Create'));
+
+        return $form;
+    }
+
+    /**
+     * Displays a form to create a new Organization entity.
+     *
+     */
+    public function newAction()
+    {
+        $entity = new Organization();
+        $form   = $this->createCreateForm($entity);
+
+        return $this->render('AcmebsceneBundle:Organization:new.html.twig', array(
+            'entity' => $entity,
+            'form'   => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Organization entity.
+     *
+     */
+    public function showAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmebsceneBundle:Organization')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Organization entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AcmebsceneBundle:Organization:show.html.twig', array(
+            'entity'      => $entity,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Organization entity.
+     *
+     */
+    public function editAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmebsceneBundle:Organization')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Organization entity.');
+        }
+
+        $editForm = $this->createEditForm($entity);
+        $deleteForm = $this->createDeleteForm($id);
+
+        return $this->render('AcmebsceneBundle:Organization:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+    * Creates a form to edit a Organization entity.
+    *
+    * @param Organization $entity The entity
+    *
+    * @return \Symfony\Component\Form\Form The form
+    */
+    private function createEditForm(Organization $entity)
+    {
+        $form = $this->createForm(new OrganizationType(), $entity, array(
+            'action' => $this->generateUrl('organization_update', array('id' => $entity->getId())),
+            'method' => 'PUT',
+        ));
+
+        $form->add('submit', 'submit', array('label' => 'Update'));
+
+        return $form;
+    }
+    /**
+     * Edits an existing Organization entity.
+     *
+     */
+    public function updateAction(Request $request, $id)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AcmebsceneBundle:Organization')->find($id);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Organization entity.');
+        }
+
+        $deleteForm = $this->createDeleteForm($id);
+        $editForm = $this->createEditForm($entity);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isValid()) {
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('organization_edit', array('id' => $id)));
+        }
+
+        return $this->render('AcmebsceneBundle:Organization:edit.html.twig', array(
+            'entity'      => $entity,
+            'edit_form'   => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+    /**
+     * Deletes a Organization entity.
+     *
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $form = $this->createDeleteForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $entity = $em->getRepository('AcmebsceneBundle:Organization')->find($id);
+
+            if (!$entity) {
+                throw $this->createNotFoundException('Unable to find Organization entity.');
+            }
+
+            $em->remove($entity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('organization'));
+    }
+
+    /**
+     * Creates a form to delete a Organization entity by id.
+     *
+     * @param mixed $id The entity id
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm($id)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('organization_delete', array('id' => $id)))
+            ->setMethod('DELETE')
+            ->add('submit', 'submit', array('label' => 'Delete'))
+            ->getForm()
+        ;
+    }
 }
---
+++ src/Acme/bsceneBundle/Form/OrganizationType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acme\bsceneBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class OrganizationType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('website')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\bsceneBundle\Entity\Organization'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'acme_bscenebundle_organization';
+    }
+}
