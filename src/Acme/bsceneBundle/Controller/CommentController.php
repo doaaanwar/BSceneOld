968c7bc4d9a734ab8ee899cf276c950952e78bdc
doaa <doaa@localhost>
2015-03-19 17:49:13 -0400

    start the comment page for admin

--- src/Acme/bsceneBundle/Controller/CommentController.php
+++ src/Acme/bsceneBundle/Controller/CommentController.php
@@ -20,11 +20,32 @@ use Symfony\Component\HttpFoundation\Session\Session;
 class CommentController extends Controller
 {
     /*
-     * function that get the list of newest comments for the admin
+     * function that get the list of new comment and go to the comment list admin page
      */
-    public function indexAction()
+    public function adminIndexAction()
     {
-     
+        $em = $this->getDoctrine()->getEntityManager();
+        $repository = $em->getRepository('\Acme\bsceneBundle\Entity\EventComments');
+
+        
+        //$commentList = $repository->findBy(array('commentTime' >= $lastLogin));
+        
+        $commentList = $repository->findAll();
+        
+        if(count($commentList) > 0)
+        {
+        
+            return $this->render('AcmebsceneBundle:EventComments:commentList.html.twig',array('commnentList' => $organizationList));
+        }
+        else 
+        {
+            return $this->render('AcmebsceneBundle:EventComments:commentList.html.twig',array('commentList' => NULL,'errormessage' => "No new comment found from your last login"));
+
+        }
     }
+    
+    
+   
+    
     
 }
