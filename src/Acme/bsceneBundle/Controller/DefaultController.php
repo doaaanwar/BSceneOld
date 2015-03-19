8dd23f2bbd352261c92efc46b715978c2bf81ab5
doaa <doaa@localhost>
2015-03-19 16:03:15 -0400

    final update on login

--- src/Acme/bsceneBundle/Controller/DefaultController.php
+++ src/Acme/bsceneBundle/Controller/DefaultController.php
@@ -24,45 +24,52 @@ class DefaultController extends Controller
     {
         
         if($request->getMethod()=='POST'){
-            //$session = $request->getSession();
-            //$session->clear();
+            $session = $request->getSession();
+            $session->clear();
             $username = $request->get('username');
             $password = $request->get('password');
             $em = $this->getDoctrine()->getEntityManager();
             
-            
-           
-            
+    
             
             $repository = $em->getRepository('\Acme\bsceneBundle\Entity\Account');
             
-
+            //fetch user by username
             $user = $repository->findOneBy(array('username'=>$username));
-            $verified = password_verify($password, $user->getPassword());
-            if($user && $verified)
+            
+            //username found
+            if($user)
             {
-                $session = new Session();
-                $session->start();
-                $session->invalidate(50);
-                $session->set('member',$user->getUsername());
-                $session->set('memberId',$user->getId());
-                if($user->getIsAdmin() == 1)
+                $verified = password_verify($password, $user->getPassword());
+                if($verified)
                 {
-                    
-                    $session->set('admin','admin');
+                    //$session = new Session();
+                    //$session->start();
+                    $session->invalidate(50);
+                    $session->set('member',$user->getUsername());
+                    $session->set('memberId',$user->getId());
+                    if($user->getIsAdmin() == 1)
+                    {
+
+                        $session->set('admin','admin');
+                    }
+
+                     
+                     return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => $user->getUsername()));
                 }
-               
-                 //$user->getRoles();
-                 return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => $user->getUsername()));
+                else
+                {
+                    //password doesn't match
+                   return $this->render('AcmebsceneBundle:Default:index.html.twig',array('errormessage' => 'uncorrect password'));
+                }
             }
             else
             {
-                 return $this->render('AcmebsceneBundle:Default:index.html.twig',array('name' => 'login failed'));
+                 
+                 return $this->render('AcmebsceneBundle:Default:index.html.twig',array('errormessage' => 'login failed'));
              
             }
         }
-       
-       
         else
         {
              return $this->render('AcmebsceneBundle:Default:index.html.twig');
